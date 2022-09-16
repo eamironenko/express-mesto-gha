519f1ee164d40d14f912dedbb891e0cf2d459398
unknown <eam618334@yandex.ru>
2022-09-16 11:07:34 +0300

    fix: отладть localhost

--- middlewares/auth.js
+++ middlewares/auth.js
@@ -1,3 +1,4 @@
+const { NODE_ENV, JWT_SECRET } = process.env;
 const jwt = require('jsonwebtoken');
 const AuthErr = require('../errors/AuthErr');
 
@@ -14,7 +15,8 @@ const auth = (req, res, next) => {
 
   // верифицируем токен
   try {
-    payload = jwt.verify(token, 'some-secret-key');
+    // payload = jwt.verify(token, 'some-secret-key');
+    payload = jwt.verify(token, NODE_ENV === 'production' ? JWT_SECRET : 'some-secret-key');
   } catch (err) {
     throw new AuthErr('Нужно авторизоваться');
   }
