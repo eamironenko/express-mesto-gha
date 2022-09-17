1d3221e91b0bbb51c9dbbf9bd20ccaa79533a3a1
unknown <eam618334@yandex.ru>
2022-09-17 21:48:34 +0300

    fix: отладть код

--- controllers/users.js
+++ controllers/users.js
@@ -5,7 +5,7 @@ const NotFoundPage = require('../errors/NotFoundPage');
 const Validation = require('../errors/Validation');
 const UniqueErr = require('../errors/UniqueErr');
 
-// const { NODE_ENV, JWT_SECRET } = process.env;
+const { NODE_ENV, JWT_SECRET } = process.env;
 
 module.exports.getUsers = (req, res, next) => {
   User.find({})
@@ -110,12 +110,12 @@ module.exports.login = (req, res, next) => {
   return User.findUserByCredentials(email, password)
     .then((user) => {
       // создаем
-      const token = jwt.sign({ _id: user._id }, 'some-secret-key', { expiresIn: '7d' });
-      // const token = jwt.sign(
-      // { _id: user._id },
-      // NODE_ENV === 'production' ? JWT_SECRET : 'dev-secret',
-      // { expiresIn: '7d' },
-      // );
+      // const token = jwt.sign({ _id: user._id }, 'some-secret-key', { expiresIn: '7d' });
+      const token = jwt.sign(
+        { _id: user._id },
+        NODE_ENV === 'production' ? JWT_SECRET : 'dev-secret',
+        { expiresIn: '7d' },
+      );
       res.send({ token });
     })
     .catch((err) => next(err));
